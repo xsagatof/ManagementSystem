f050c8a989691d86d538af2ab6b71a2cb935ca41
xsagatof <Ultimate123443@gmail.com>
2024-10-21 23:50:31 +0500

    Added functionality to Employee (geting started with changes in git/github project)

--- Employee.cs
+++ Employee.cs
@@ -8,6 +8,46 @@ namespace ManagementSystem
 {
 	public abstract class Employee
 	{
-		
+		public string Name;
+
+		public int Id;
+
+		public float Salary;
+
+		public Employee(string name, int id, float salary)
+		{
+			Name = name;
+			Id = id;
+			Salary = salary;
+		}
+
+		public void setName(string name)
+		{
+			Name = name;
+		}
+		public void setId(int id)
+		{
+			Id = id;
+		}
+		public void setSalary(float salary)
+		{
+			Salary = salary;
+		}
+
+		public string getName()
+		{
+			return Name;
+		}
+		public int getId()
+		{
+			return Id;
+		}
+		public float getSalary()
+		{
+			return Salary;
+		}
+
+		public abstract double calculateSalary();
+		public abstract void displayEmployeeDetails();
 	}
 }
