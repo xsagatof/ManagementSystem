fcce46492d9c52f152b4540edd240f272351e3cc
xsagatof <Ultimate123443@gmail.com>
2024-10-24 23:13:16 +0500

    added new features

--- Employees/Intern.cs
+++ Employees/Intern.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace ManagementSystem.Employees
 {
-    internal class Intern
+    public class Intern : Employee
     {
+	    public Intern(string name, int id, float salary) : base(name, id, salary)
+	    {
+		    baseSalary = salary;
+		    this.name = name;
+		    this.id = id;
+	    }
 
-    }
+	    public override double calculateSalary()
+	    {
+		    return baseSalary;
+	    }
+
+	    public override void displayEmployeeDetails()
+	    {
+		    Console.WriteLine("Name: " + name);
+		    Console.WriteLine("ID: " + id);
+		    Console.WriteLine("Salary: " + baseSalary);
+	    }
+	}
 }
