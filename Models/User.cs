363fed0626e1b34749126ffad9e476e19b349176
xsagatof <Ultimate123443@gmail.com>
2025-11-11 23:55:12 +0500

    Start of changing the structure

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Role { get; set; }
+		public double Salary { get; set; }
+	}
+}
