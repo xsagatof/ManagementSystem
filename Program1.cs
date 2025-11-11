551075bcf6279d9918b632ee0a2809a163170af5
xsagatof <Ultimate123443@gmail.com>
2025-11-11 23:56:46 +0500

    adding database into project

---
+++ Program1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagementSystem.Database;
+
+namespace ManagementSystem
+{
+	public class Program1
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
