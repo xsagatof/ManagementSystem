5c038849fe96ce0446ab3e567d1c076fb08110c8
xsagatof <Ultimate123443@gmail.com>
2025-11-12 23:56:52 +0500

    Add UserRole enum for better user role management

---
+++ Models/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Models
+{
+	public enum UserRole
+	{
+		Admin,
+		Teacher,
+		Stuff,
+		Student
+	}
+}
