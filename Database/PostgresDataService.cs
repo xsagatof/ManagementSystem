e9fe540a48ff569bfaefa700e21c1f074ba4db48
xsagatof <Ultimate123443@gmail.com>
2025-11-24 23:58:50 +0500

    minor changes

--- Database/PostgresDataService.cs
+++ Database/PostgresDataService.cs
@@ -1,6 +1,8 @@
 ﻿using ManagementSystem.Models;
+using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -17,7 +19,31 @@ namespace ManagementSystem.Database
 			_connectionString = connectionString;
 		}
 
-		public  List<Student> LoadStudents() { }
+		public  List<Student> LoadStudents() 
+		{
+			var students = new List<Student>();
+
+			using var connection = new NpgsqlConnection(_connectionString);
+			connection.Open();
+
+			var sql = "SELECT * FROM students";
+			using var command = new NpgsqlCommand(sql, connection);
+			using var reader = command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				var student = new Student()
+				{
+					StudentId = reader.GetInt32("studentid"),
+					Fullname = reader.GetString("fullname"),
+					Age = reader.GetInt32("age"),
+					Email = reader.GetString("email"),
+					Faculty = reader.GetString("faculty")
+				};
+				students.Add(student);
+			}
+			return students;
+		}
 		public  void SaveStudent(Student student) { }
 		public  void UpdateStudent(Student student) { }
 		public  void DeleteStudent(int id) { }
